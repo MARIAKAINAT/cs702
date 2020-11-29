95ae8434e42a8daf5e72efdc6d07634bba79e69a
MARIAKAINAT <mariakainat33@gmail.com>
2020-11-29 22:48:21 +0500

    all

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+class Ifelse{
+  public static void main(String[] args) {
+
+    int number = 5;
+
+    if (number > 0) {
+      System.out.println("positive number");
+    }
+ 	 else  	 	
+ 	 	System.out.println("negative number");
+ 	 
+  }
+}
---
+++ programone.java
@@ -0,0 +1,13 @@
+public class programone {
+  public static void main(String[] args) {
+    int num;
+    int num1;
+    num = 2; 
+    num1 = ++num;
+    int sum;
+    sum = num+num1;
+
+    System.out.println(sum);
+  }
+}
+
